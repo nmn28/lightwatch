50807795d17166cef1860104241ac0b81922b6f3
Nicholas Nelson <nicholasmnelson10@gmail.com>
2024-09-21 14:52:18 -0700

    Initial Commit

---
+++ lightwatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lightwatch Watch App
+//
+//  Created by Nicholas Nelson on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lightwatch Watch App/lightwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightwatchApp.swift
+//  lightwatch Watch App
+//
+//  Created by Nicholas Nelson on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct lightwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
